1e697520967d0585392ecad7a68f075e1701d591
Dobrovolsky Maxim <isip_m.ya.dobrovolosky@mpt.ru>
2023-05-11 22:34:42 +0300

    Base docker

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -37,9 +37,9 @@ import { envValidationSchema } from './envValidationSchema';
         {
           type: 'postgres',
           host: configService.get<string>('POSTGRES_HOST'),
-          port: configService.get<number>('POSTGRESS_PORT'),
+          port: configService.get<number>('POSTGRES_PORT'),
           username: configService.get<string>('POSTGRES_USER'),
-          password: configService.get<string>('POSTGRESS_PASSWORD'),
+          password: configService.get<string>('POSTGRES_PASSWORD'),
           database: configService.get<string>('POSTGRES_DB'),
           entities: [User, PhoneVerification, TipLink, TipLinkData, Payment, Organization, Employee, Payout],
           synchronize: true, // delete in production
